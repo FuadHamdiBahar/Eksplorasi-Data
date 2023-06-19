26fce6432f888cea1fe13c37aba5c98f6c493c32
Fuad Hamdi Bahar <fuadhamdi99@gmail.com>
2023-06-19 13:36:10 +0800

    edit sedikit

--- 25. Menggabungkan tabel.sql
+++ 25. Menggabungkan tabel.sql
@@ -10,6 +10,7 @@ SELECT pt.id, pt.nama_pt, ps.nama_prodi
 FROM perguruan_tinggi pt LEFT JOIN program_studi ps 
 ON pt.id = ps.id_pt;
 
+-- Challange 
 SELECT pt.id, pt.nama_pt, ps.nama_prodi 
 FROM perguruan_tinggi pt RIGHT JOIN program_studi ps 
 ON pt.id = ps.id_pt;
