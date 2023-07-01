d47722b449d416240eb8b53b340a4d9859ce3b01
Fuad Hamdi Bahar <fuadhamdi99@gmail.com>
2023-07-01 22:13:05 +0800

    Deleted files

--- 27. LIMIT, ORDER BY and COUNT in MySQL.sql
+++
@@ -1,5 +0,0 @@
-select id, nama_pt from perguruan_tinggi pt limit 3
-
-select id, nama_pt from perguruan_tinggi pt order by nama_pt ASC;
-
-select COUNT(*) from perguruan_tinggi pt where nama_pt like '%Universitas%'
